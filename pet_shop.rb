4bf3ae6e78f0cb63f806b0037c6bac131752d313
Ross Crichton <rcrichton87@googlemail.com>
2017-02-03 10:28:56 +0000

    Added total_cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,7 @@
-def pet_shop_name(pet_shop)
-  shop_name = pet_shop[:name]
+def pet_shop_name(pet_shop_hash)
+  shop_name = pet_shop_hash[:name]
+end
+
+def total_cash(pet_shop_hash)
+  cash = pet_shop_hash[:admin][:total_cash]
 end
