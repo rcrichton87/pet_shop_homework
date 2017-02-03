a5782e506f9c081eea55b9432f9355977c94ed0a
Ross Crichton <rcrichton87@googlemail.com>
2017-02-03 10:33:47 +0000

    added add_or_remove_cash function

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,7 @@ end
 def total_cash(pet_shop_hash)
   cash = pet_shop_hash[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop_hash, cash_change)
+  new_cash = pet_shop_hash[:admin][:total_cash] += cash_change
+end
